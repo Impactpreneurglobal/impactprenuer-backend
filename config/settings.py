92465d9b3bd1d992ea8089d06cc70eb4de73a454
danimpulse500 <danimpulse500@gmail.com>
2025-12-31 10:33:59 +0100

    updated db

--- config/settings.py
+++ config/settings.py
@@ -92,12 +92,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+
+else:
+    DATABASES = {
+        'default': env.db("DATABASE_URL"),
     }
-}
 
 # ── DRF Settings ───────────────────────────────────────────────
 REST_FRAMEWORK = {
